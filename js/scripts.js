7b1a7adadf65c0406d42630fc94e2dc0b9c4f8c7
tinacai1997 <tinacai1997@gmail.com>
2021-11-23 15:13:22 -0800

    Remove scroll lock when menu is open
    Removes scroll bar which shifts content

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,11 @@ console.log("this is a secret message");
 let menuOpen = false;
 
 function menuToggle() {
-  let getBody = document.querySelector("Body");
   let getSideMenu = document.querySelector(".side-menu");
   let getSideMenuNav = document.querySelector(".side-menu nav");
   let getMenuMask = document.querySelector(".menu-mask");
 
   if (menuOpen === false) {
-    // getBody.style.overflow = "hidden";
     getSideMenu.style.visibility = "visible";
     getSideMenu.style.width = "25vw";
     getSideMenuNav.style.opacity = "1";
@@ -19,7 +17,6 @@ function menuToggle() {
     menuOpen = true;
 
   } else {
-    // getBody.style.overflow = "visible";
     getSideMenu.style.visibility = "hidden";
     getSideMenu.style.width = "0%";
     getSideMenuNav.style.opacity = "0";
