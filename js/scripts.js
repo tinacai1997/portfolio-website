08dfea14e54a656a45f67919db4ce151c4638635
tinacai1997 <tinacai1997@gmail.com>
2021-11-23 15:57:51 -0800

    Add contact links to side menu and add css transitions

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,16 @@ let menuOpen = false;
 function menuToggle() {
   let getSideMenu = document.querySelector(".side-menu");
   let getSideMenuNav = document.querySelector(".side-menu nav");
+  let getSideContact = document.querySelector(".side-contact");
   let getMenuMask = document.querySelector(".menu-mask");
 
   if (menuOpen === false) {
     getSideMenu.style.visibility = "visible";
     getSideMenu.style.width = "25vw";
+    getSideMenuNav.style.visibility = "visible";
     getSideMenuNav.style.opacity = "1";
+    getSideContact.style.visibility = "visible";
+    getSideContact.style.opacity = "1";
     getMenuMask.style.visibility = "visible";
     getMenuMask.style.opacity = "0.5";
 
@@ -19,7 +23,10 @@ function menuToggle() {
   } else {
     getSideMenu.style.visibility = "hidden";
     getSideMenu.style.width = "0%";
+    getSideMenuNav.style.visibility = "hidden";
     getSideMenuNav.style.opacity = "0";
+    getSideContact.style.visibility = "hidden";
+    getSideContact.style.opacity = "0";
     getMenuMask.style.visibility = "hidden";
     getMenuMask.style.opacity = "0";
 
