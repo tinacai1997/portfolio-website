9a157378f99d9d52c78cbdd013d2b8e7e04c5ddb
tinacai1997 <tinacai1997@gmail.com>
2021-11-23 13:54:05 -0800

    Add sidebar scripts and css transitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
 console.log("this is a secret message");
+
+let menuOpen = false;
+
+function menuToggle() {
+  let getSideMenu = document.querySelector(".side-menu");
+  let getSideMenuNav = document.querySelector(".side-menu nav")
+  let getMenuMask = document.querySelector(".menu-mask")
+
+  if (menuOpen === false) {
+    getSideMenu.style.visibility = "visible";
+    getSideMenu.style.width = "25vw";
+    getSideMenuNav.style.opacity = "1";
+
+    menuOpen = true;
+
+  } else {
+    getSideMenu.style.visibility = "hidden";
+    getSideMenu.style.width = "0%";
+    getSideMenuNav.style.opacity = "0";
+
+    menuOpen = false;
+  }
+}
