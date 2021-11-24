640ea1d294cebcdb60179897121b8bdf51c3256a
tinacai1997 <tinacai1997@gmail.com>
2021-11-23 16:42:54 -0800

    Adjust sidebar width for different screen widths

--- js/scripts.js
+++ js/scripts.js
@@ -10,23 +10,34 @@ function menuToggle() {
 
   if (menuOpen === false) {
     getSideMenu.style.visibility = "visible";
-    getSideMenu.style.width = "25vw";
+
     getSideMenuNav.style.visibility = "visible";
     getSideMenuNav.style.opacity = "1";
+
     getSideContact.style.visibility = "visible";
     getSideContact.style.opacity = "1";
+
     getMenuMask.style.visibility = "visible";
     getMenuMask.style.opacity = "0.5";
+
+    if (screen.width <= 640px) {
+      getSideMenu.style.width = "50vw";
+    } else {
+      getSideMenu.style.width = "35vw";
+    }
 
     menuOpen = true;
 
   } else {
     getSideMenu.style.visibility = "hidden";
     getSideMenu.style.width = "0%";
+
     getSideMenuNav.style.visibility = "hidden";
     getSideMenuNav.style.opacity = "0";
+
     getSideContact.style.visibility = "hidden";
     getSideContact.style.opacity = "0";
+
     getMenuMask.style.visibility = "hidden";
     getMenuMask.style.opacity = "0";
 
