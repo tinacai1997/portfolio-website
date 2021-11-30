d33c258c7e175bb638deb82bcd93df954d42996f
tinacai1997 <tinacai1997@gmail.com>
2021-11-30 15:33:51 -0800

    Close side menu when clicking outside

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,67 @@
 console.log("this is a secret message");
 
 
-// Toggle Side Menu
+// --------------------- Toggle Side Menu --------------------//
+
+// Get variables
 
 let menuOpen = false;
+let getSideMenu = document.querySelector(".side-menu");
+let getSideMenuNav = document.querySelector(".side-menu nav");
+let getSideContact = document.querySelector(".side-contact");
+let getMenuMask = document.querySelector(".menu-mask");
+let getMenuIcon = document.querySelector(".menu-icon");
 
-function menuToggle() {
-  let getSideMenu = document.querySelector(".side-menu");
-  let getSideMenuNav = document.querySelector(".side-menu nav");
-  let getSideContact = document.querySelector(".side-contact");
-  let getMenuMask = document.querySelector(".menu-mask");
-  let getMenuIcon = document.querySelector(".menu-icon");
+// Open Menu
+
+function openMenu() {
+  getSideMenu.style.visibility = "visible";
+  getSideMenu.style.transform = "scaleX(1)";
+
+  getSideMenuNav.style.visibility = "visible";
+  getSideMenuNav.style.opacity = "1";
+
+  getSideContact.style.visibility = "visible";
+  getSideContact.style.opacity = "1";
+
+  getMenuMask.style.visibility = "visible";
+  getMenuMask.style.opacity = "0.5";
+
+  menuOpen = true;
 
   getMenuIcon.classList.toggle('close');
   getMenuIcon.classList.toggle('open');
+}
+
+// Close Menu
+
+function closeMenu() {
+ 
+  getSideMenu.style.visibility = "hidden";
+  getSideMenu.style.transform = "scaleX(0)";
+
+  getSideMenuNav.style.visibility = "hidden";
+  getSideMenuNav.style.opacity = "0";
+
+  getSideContact.style.visibility = "hidden";
+  getSideContact.style.opacity = "0";
+
+  getMenuMask.style.visibility = "hidden";
+  getMenuMask.style.opacity = "0";
+
+  menuOpen = false;
+
+  getMenuIcon.classList.toggle('close');
+  getMenuIcon.classList.toggle('open');
+}
+
+function menuToggle() {
 
   if (menuOpen === false) {
-    getSideMenu.style.visibility = "visible";
-    getSideMenu.style.transform = "scaleX(1)";
-
-    getSideMenuNav.style.visibility = "visible";
-    getSideMenuNav.style.opacity = "1";
-
-    getSideContact.style.visibility = "visible";
-    getSideContact.style.opacity = "1";
-
-    getMenuMask.style.visibility = "visible";
-    getMenuMask.style.opacity = "0.5";
-
-    menuOpen = true;
-
+    openMenu();
   } else {
-    getSideMenu.style.visibility = "hidden";
-    getSideMenu.style.transform = "scaleX(0)";
-
-    getSideMenuNav.style.visibility = "hidden";
-    getSideMenuNav.style.opacity = "0";
-
-    getSideContact.style.visibility = "hidden";
-    getSideContact.style.opacity = "0";
-
-    getMenuMask.style.visibility = "hidden";
-    getMenuMask.style.opacity = "0";
-
-    menuOpen = false;
-  }
+    closeMenu();
+  }  
 
 }
 
