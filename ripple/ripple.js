42df85377e58a15b50f995777af0c738d262b5b7
tinacai1997 <tinacai1997@gmail.com>
2021-12-08 02:52:37 -0800

    Add ripple button css and js

---
+++ ripple/ripple.js
@@ -0,0 +1,18 @@
+$(function() {
+    $(".ripple-button > a").on('mouseenter', function(e) {
+      x = e.pageX - $(this).offset().left;
+      y = e.pageY - $(this).offset().top;
+      $(this).find("span").css({
+        top: y,
+        left: x
+      });
+    });
+    $(".ripple-button > a").on('mouseout', function(e) {
+      x = e.pageX - $(this).offset().left;
+      y = e.pageY - $(this).offset().top;
+      $(this).find("span").css({
+        top: y,
+        left: x
+      });
+    });
+  });
