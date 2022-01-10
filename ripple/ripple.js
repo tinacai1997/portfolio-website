d372821a13903ee49b2ed21e9d1e0b2ca44bd0f5
tinacai1997 <tinacai1997@gmail.com>
2022-01-09 23:05:49 -0800

    Update about page to match mobile mockups
    Contact section

--- ripple/ripple.js
+++ ripple/ripple.js
@@ -15,4 +15,20 @@ $(function() {
         left: x
       });
     });
+    $(".ripple-button > button").on('mouseenter', function(e) {
+      x = e.pageX - $(this).offset().left;
+      y = e.pageY - $(this).offset().top;
+      $(this).find("span").css({
+        top: y,
+        left: x
+      });
+    });
+    $(".ripple-button > button").on('mouseout', function(e) {
+      x = e.pageX - $(this).offset().left;
+      y = e.pageY - $(this).offset().top;
+      $(this).find("span").css({
+        top: y,
+        left: x
+      });
+    });
   });
